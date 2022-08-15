4435516216c902bee7865496cdc3f754484b86b5
EladBil <bilelad@gmail.com>
2022-08-15 19:10:15 +0300

    part basic model

--- UsefullFunctions.py
+++ UsefullFunctions.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pylab as plt
+import numpy as np
+import seaborn as sns
 
 
 """
@@ -13,3 +16,63 @@ def load_dataset():
     df2 = pd.read_csv(second_dest_file_path)
     df = pd.concat([df1, df2])
     return df
+
+"""
+A method to load a confusion matrix plot.
+"""
+def conf_matrix(cf_matrix):
+    group_names = ['True Neg','False Pos','False Neg','True Pos']
+    group_counts = ["{0:0.0f}".format(value) for value in
+                    cf_matrix.flatten()]
+
+    group_percentages = ["{0:.2%}".format(value) for value in
+                         cf_matrix.flatten()/np.sum(cf_matrix)]
+
+    labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in
+              zip(group_names,group_counts,group_percentages)]
+
+    labels = np.asarray(labels).reshape(2,2)
+
+    ax = sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues')
+
+    ax.set_title('Confusion Matrix\n')
+    ax.set_xlabel('\nPredicted Values')
+    ax.set_ylabel('Actual Values ')
+
+    ## Ticket labels - List must be in alphabetical order
+    ax.xaxis.set_ticklabels(['False','True'])
+    ax.yaxis.set_ticklabels(['False','True'])
+
+    ## Display the visualization of the Confusion Matrix.
+    plt.show()
+
+
+"""
+A method to load a Precision-Recall plot.
+"""
+def plotPR(precision, recall):
+    plt.figure()
+    plt.plot(recall, precision, label='PR curve')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision Recall curve')
+    plt.legend(loc='lower left')
+    plt.show()
+
+
+"""
+A method to load a AUC plot.
+"""
+def plotRoc(fpr, tpr, auc):
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (AUC = %0.2f)' % auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic- ROC')
+    plt.legend(loc="lower right")
+    plt.show()
